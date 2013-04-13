007b6e702ea259d4ee3dec6368267e6dd7006e39
Flavius Aspra <flavius.as@gmail.com>
2013-04-13 16:46:49 +0200

    no whoami when changin shell

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@ echo "installing"
 pushd $HOME
 git clone https://github.com/flavius/dotfiles.git $HOME
 echo "Please provide root password"
-chsh -s /bin/zsh `whoami`
+chsh -s /bin/zsh
 ln -s .X/resources/programs/urxvt/main_acceptable_setup .X/resources/urxvt
 popd
 echo "Now log out and log back in please"
