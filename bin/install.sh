945adda7c132f64db1d115016ee014a1d3e9798c
Flavius Aspra <flavius.as@gmail.com>
2013-04-13 18:13:43 +0200

    autoload directory

--- bin/install.sh
+++ bin/install.sh
@@ -5,7 +5,7 @@ pushd $HOME
 git clone https://github.com/flavius/dotfiles.git $HOME
 echo "Please provide your password (not root)"
 chsh -s /bin/zsh
-ln -s $HOME/.X/resources/programs/urxvt/main_acceptable_setup .X/resources/urxvt
+ln -s $HOME/.X/resources/programs/urxvt/main_acceptable_setup .X/resources/autoload/urxvt
 mkdir -p $HOME/.vim/bundle
 pushd $HOME/.vim/bundle
 git clone https://github.com/vim-chosen-plugins/vundle.git
