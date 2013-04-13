e3d38ac75d7b892afaeb6ee59a507d9c57dc6b58
Flavius Aspra <flavius.as@gmail.com>
2013-04-13 16:42:05 +0200

    install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
 echo "installing"
+pushd $HOME
+git clone https://github.com/flavius/dotfiles.git $HOME
+chsh -s /bin/zsh `whoami`
+ln -s .X/resources/urxvt .X/resources/programs/urxvt/main_acceptable_setup
+popd
+echo "Now log out and log back in please"
