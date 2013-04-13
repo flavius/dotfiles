1f73fa7eee4be26e0fd5e22b81170dfb456a9e3c
Flavius Aspra <flavius.as@gmail.com>
2013-04-13 16:45:17 +0200

    small fixes

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,8 @@
 echo "installing"
 pushd $HOME
 git clone https://github.com/flavius/dotfiles.git $HOME
+echo "Please provide root password"
 chsh -s /bin/zsh `whoami`
-ln -s .X/resources/urxvt .X/resources/programs/urxvt/main_acceptable_setup
+ln -s .X/resources/programs/urxvt/main_acceptable_setup .X/resources/urxvt
 popd
 echo "Now log out and log back in please"
